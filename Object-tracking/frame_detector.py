3cabcf62890c7d8aadaab7e6bdb91c525260fcaf
BernardWong97 <ben.wc88@gmail.com>
2020-02-20 04:01:22 +0000

    Refactor code, migrate video capture to web api

--- Object-tracking/frame_detector.py
+++ Object-tracking/frame_detector.py
@@ -3,45 +3,35 @@ from PIL import Image
 from imageai.Detection import ObjectDetection
 from centroid_tracker import CentroidTracker
 
-# Instantiate detector and activate webcam
-detector = ObjectDetection()
-video_capture = cv2.VideoCapture(0)
-video_capture.set(3, 960)
-video_capture.set(4, 540)
-video_capture.set(cv2.CAP_PROP_AUTOFOCUS, 0)
 
-# Set and load model
-model_path = "models/yolo.h5"
-# input_path = "./input/test4.png"
-# output_path = "./output/results4.png"
-detector.setModelTypeAsYOLOv3()
-detector.setModelPath(model_path)
-detector.loadModel()
+class LiveDetector:
+    def __init__(self):
+        # Instantiate detector
+        self.detector = ObjectDetection()
 
-# Set custom objects
-custom_objects = detector.CustomObjects(car=True, motorcycle=True, person=True, bicycle=True, dog=True)
+        # Set and load model
+        self.model_path = "models/yolo.h5"
+        self.detector.setModelTypeAsYOLOv3()
+        self.detector.setModelPath(self.model_path)
+        self.detector.loadModel()
 
-# Image object detection
-# detections = detector.detectObjectsFromImage(input_image=input_path, output_image_path=output_path)
+        # Set custom objects
+        self.custom_objects = self.detector.CustomObjects(car=True,
+                                                          motorcycle=True,
+                                                          person=True,
+                                                          bicycle=True,
+                                                          dog=True)
+        self.tracker = CentroidTracker()
 
-tracker = CentroidTracker()
-
-# Webcam live object detection
-# While loop webcam each frame
-while True:
-    # Capture frame-by-frame
-    ret, frame = video_capture.read()
-
-    # If capture returns true
-    if ret:
+    def track_objects(self, frame):
         rects = []
         text = ""
         x, y = 0, 0
         img = Image.fromarray(frame)
-        returned_image, detection = detector.detectCustomObjectsFromImage(custom_objects=custom_objects,
-                                                                          input_image=img,
-                                                                          output_type="array",
-                                                                          input_type="array")
+        returned_image, detection = self.detector.detectCustomObjectsFromImage(custom_objects=self.custom_objects,
+                                                                               input_image=img,
+                                                                               output_type="array",
+                                                                               input_type="array")
 
         for eachObject in detection:
             rects.append(eachObject["box_points"])
@@ -50,24 +40,16 @@ while True:
             cv2.rectangle(frame, (startX, startY), (endX, endY), (0, 255, 0), 2)
             text = eachObject["name"]
 
-        objects = tracker.update(rects)
+        objects = self.tracker.update(rects)
 
         if objects is not None:
             for (objectID, centroid) in objects.items():
                 # draw both the ID of the object and the centroid of the
                 # object on the output frame
-                if tracker.disappeared[objectID] < 1:
+                if self.tracker.disappeared[objectID] < 1:
                     text += " " + str(objectID)
                     cv2.putText(frame, text, (centroid[0] - 30, centroid[1] - 10),
                                 cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0, 255, 0), 2)
                     cv2.circle(frame, (centroid[0], centroid[1]), 4, (0, 255, 0), -1)
 
-        cv2.imshow("Frame", frame)
-
-    # Press q to end feed
-    if cv2.waitKey(1) & 0xFF == ord('q'):
-        break
-
-# When everything is done, release the capture
-video_capture.release()
-cv2.destroyAllWindows()
+        return frame
--- Web-API/app.py
+++ Web-API/app.py
@@ -1,11 +1,64 @@
-from flask import Flask, render_template
+import sys
 
+sys.path.append("../Object-tracking")
+from frame_detector import LiveDetector
+from flask import Flask, render_template, Response
+import cv2
+
+outputFrame = None
+
+# Initialize flask
 app = Flask("__main__")
+
+video_capture = cv2.VideoCapture(0)
+video_capture.set(3, 960)
+video_capture.set(4, 540)
+video_capture.set(cv2.CAP_PROP_AUTOFOCUS, 0)
+
+
+def track():
+    global video_capture, outputFrame
+
+    # Webcam live object detection
+    # While loop webcam each frame
+    while True:
+        # Capture frame-by-frame
+        ret, frame = video_capture.read()
+
+        # If capture returns true
+        if ret:
+            outputFrame = frame.copy()
+
+
+def generate():
+    global outputFrame
+
+    while True:
+        if outputFrame is None:
+            continue
+
+        (ret, encodedImg) = cv2.imencode(".jpg", outputFrame)
+
+        if not ret:
+            continue
+
+        yield (b'--frame\r\n' b'Content-Type: image/jpeg\r\n\r\n' +
+               bytearray(encodedImg) + b'\r\n')
 
 
 @app.route("/")
-def home_page():
-    return render_template("index.html", token="Test Flask + React")
+def index():
+    return render_template("index.html")
 
 
-app.run(debug=True)
+@app.route("/video_feed")
+def video_feed():
+    track()
+    return Response(generate(),
+                    mimetype="multipart/x-mixed-replace; boundary=frame")
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
+
+video_capture.release()
